9fe0cdc65cc0231dd3d59047ce7247624eb10bf2
LucasSalesMariga <61717240+LucasSalesMariga@users.noreply.github.com>
2020-04-23 15:57:33 -0300

    Styled Component

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { View } from 'react-native';
 
+import {Container} from './styles';
+
 export default function Main() {
-  return (
-    <View />
-  );
+  return <Container />;
 }
 
 Main.navigationOptions = {
-title: 'Paulinho pegador',
+title: 'Usuários',
 };

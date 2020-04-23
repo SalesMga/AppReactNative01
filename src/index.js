5ad50eb21b02978707476a7878965d4d873086c1
LucasSalesMariga <61717240+LucasSalesMariga@users.noreply.github.com>
2020-04-23 15:00:35 -0300

    Ambiente, Criando projeto, Eslint-Prettier-EditorConfig, configurando reactotron, React Navigation

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './config/ConfigReactotron';
+//faça import depois do reactotron
+import Routes from './routes';
+
+export default function App() {
+  return (
+    <Routes />
+
+  );
+}
